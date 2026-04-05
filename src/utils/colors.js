bcfe1a4ab9210822d565519074ba9bd7c204ca95
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-04-05 02:22:21 +0100

    Create colors.js

---
+++ src/utils/colors.js
@@ -0,0 +1,17 @@
+// ============================================================================
+// COLORS
+// ============================================================================
+
+export const PURPLE = '#4B0082';
+export const DPURP = '#280050';
+export const GOLD = '#D4AF37';
+export const LGOLD = '#FFEB82';
+export const DGOLD = '#A07820';
+export const GREEN = '#16A34A';
+export const LGREEN = '#DCFCE7';
+export const RED = '#DC2626';
+export const LRED = '#FEE2E2';
+export const GRAY = '#6B7280';
+export const LGRAY = '#E5E7EB';
+export const BG = '#F8F5FF';
+export const WHITE = '#FFFFFF';
