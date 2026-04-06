ea08b953bd131d71572c427b578cec20a035af08
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-04-06 02:19:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
+import ReactDOM, { createRoot } from 'react-dom/client';
+import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
+import './style.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+  <ThemeProvider>
     <App />
+  </ThemeProvider>
   </StrictMode>
 );
