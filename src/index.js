1136a863498d2186145d5ec7a491583f2564fdbe
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-04-11 14:15:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React, { StrictMode } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
+import About from './pages/About';
+import TermsOfService from './pages/TermsOfService';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 import './style.css';
 
 const rootElement = document.getElementById('root');
@@ -9,8 +13,15 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
