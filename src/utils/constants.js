79efb3c1acc805693f69b43185f0b44703ceb560
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-04-19 14:49:40 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,8 @@ export const POPOVER_AD_URL = "https://fixesconsessionconsession.com/63/ce/c2/63
 export const WA_GROUP = 'https://chat.whatsapp.com/DSeaqKuKRhw1bIFVpAJreB?mode=gi_t';
 export const WA_CHANNEL = 'https://whatsapp.com/channel/0029Vb6wPv72kNFnjr4FMr24';
 export const APP_URL = 'https://elitescholars.site';
-export const SHEETS_URL = 'https://script.google.com/macros/s/AKfycby59br8odWXDWQLkomFieaU-2aq_4wb5FiwzTiHkFgqztm0HqU5RShooJLeUANpF8sI/exec';
+export const SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwoVirYXx5sTn6BQAJTLICjSAKpyac1WMpLOAsBA19wV3F-r0iDyPScsvUDZd_KcYck/exec';
+
 
 // ============================================================================
 // ACHIEVEMENTS
