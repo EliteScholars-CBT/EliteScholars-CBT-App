3e309f9fa264fca03461d0f90c1ba2fa8de0acc8
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-07 03:27:46 +0100

    Update test.js

--- api/test.js
+++ api/test.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   return res.status(200).json({
     ok:      true,
-    message: 'API is working 🚀',
+    message: 'API is working!',
     method:  req.method,
     time:    new Date().toISOString(),
   });
