9c8ba1fc935bab1db9ec64425ffba706c7be02c5
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-07 23:43:57 +0100

    Update sheets.js

--- api/_helpers/sheets.js
+++ api/_helpers/sheets.js
@@ -2,9 +2,7 @@
 // api/_helpers/sheets.js — Apps Script communication helper
 // ============================================================================
 
-// const SHEETS_URL = process.env.SHEETS_URL;
-
-const SHEETS_URL = 'https://script.google.com/macros/s/AKfycbxmY2qZ-5zexeOLdZba1U6k3Sl7czKLzC0PjW4jP1FSO4P_mMkSWN4fUmmCBPjt09YU/exec';
+const SHEETS_URL = process.env.SHEETS_URL;
 
 export async function sheetsGet(params) {
   const url = `${SHEETS_URL}?${new URLSearchParams(params)}`;
@@ -25,5 +23,4 @@ export async function sheetsPost(body) {
     headers: { 'Content-Type': 'application/json' },
     body:    JSON.stringify(body),
   });
-console.log('got to sheets.js post' + SHEETS_URL)
 }
