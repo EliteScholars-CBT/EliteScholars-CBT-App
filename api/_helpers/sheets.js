57040735895f9ca8b947596e8acefc7b469f43f7
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-07 15:43:28 +0100

    Update sheets.js

--- api/_helpers/sheets.js
+++ api/_helpers/sheets.js
@@ -7,10 +7,16 @@
 const SHEETS_URL = 'https://script.google.com/macros/s/AKfycbxmY2qZ-5zexeOLdZba1U6k3Sl7czKLzC0PjW4jP1FSO4P_mMkSWN4fUmmCBPjt09YU/exec';
 
 export async function sheetsGet(params) {
-  // const url = `${SHEETS_URL}?${new URLSearchParams(params)}`;
-const url = `${SHEETS_URL}?action=${params.action}`
-  const res  = await fetch(url);
-  return res.json();
+  const url = `${SHEETS_URL}?${new URLSearchParams(params)}`;
+  const res = await fetch(url);
+
+  const text = await res.text();
+
+  try {
+    return JSON.parse(text);
+  } catch {
+    return text;
+  }
 }
 
 export async function sheetsPost(body) {
