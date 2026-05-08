11a3c04dd3b8cce29c0128a7c5c594b5017594d7
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-08 09:49:17 +0100

    Create debug/redis.js

---
+++ api/debug/redis.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    redis_url: process.env.UPSTASH_REDIS_REST_URL ? "set" : "missing",
+    redis_token: process.env.UPSTASH_REDIS_REST_TOKEN ? "set" : "missing"
+  });
+}
