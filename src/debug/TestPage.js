6b16cbaf8ed143042b9925669b1d86fff2a034d9
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-08 15:49:29 +0100

    Delete TestPage.js

--- src/debug/TestPage.js
+++
@@ -1,8 +0,0 @@
-import DebugConsole from "./DebugConsole";
-import useDebugAuth from "../hooks/useDebugAuth";
-
-export default function TestPage() {
-  const { logs } = useDebugAuth(true);
-
-  return <DebugConsole logs={logs} />;
-}
