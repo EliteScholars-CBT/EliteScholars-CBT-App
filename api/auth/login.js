d8f6cd4499616e2f851cf627941a34e3e29a1553
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-08 07:38:09 +0100

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   return res.status(200).json({
     stage: "backend_hash_check",
-    hash: "you88c85478aff3f3e6e94090b7b162b9331fa7ebbab84eb7f8d8824898cf60c612"
+    hash: "88c85478aff3f3e6e94090b7b162b9331fa7ebbab84eb7f8d8824898cf60c612"
   });
 }
