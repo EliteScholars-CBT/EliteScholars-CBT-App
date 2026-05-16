d88f165a6f94d0e9c757b71b2efeeb1d3408be9d
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-16 03:24:12 +0100

    Update ads.js

--- src/utils/constants/ads.js
+++ src/utils/constants/ads.js
@@ -9,10 +9,10 @@ export const POPOVER_AD_URL    = 'https://fixesconsessionconsession.com/63/ce/c2
 export const POPOVER_AD_SCRIPT = 'https://fixesconsessionconsession.com/63/ce/c2/63cec2ed9aad27f090a8f39c2b6d7469.js';
 
 export const PUBLISHER_AD_ENABLED = true;
-export const PUBLISHER_AD_SCRIPT  = 'https://fixesconsession.com/63/ce/c2/63cec2ed9aad27f090a8f39c2b6d7469.js';
+export const PUBLISHER_AD_SCRIPT  = 'https://www.highperformanceformat.com/6eb8313e3d0a4c25d0e4d2c71e7ca69d/invoke.js';
 
-export const AD_EVERY_NTH_SUBHEADING = 2;
-export const MAX_ADS_PER_PAGE        = 3;
+export const AD_EVERY_NTH_SUBHEADING = 1;
+export const MAX_ADS_PER_PAGE        = 8;
 
 export const AFFILIATE_ADS_ENABLED = true;
 export const AFFILIATE_ADS = [
