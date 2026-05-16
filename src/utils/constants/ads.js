fcb547946fe601cb1d9f535541de4ec53a3ae039
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-16 14:25:50 +0100

    Update ads.js

--- src/utils/constants/ads.js
+++ src/utils/constants/ads.js
@@ -11,8 +11,8 @@ export const POPOVER_AD_SCRIPT = 'https://fixesconsessionconsession.com/63/ce/c2
 export const PUBLISHER_AD_ENABLED = true;
 export const PUBLISHER_AD_SCRIPT  = 'https://pl29020764.effectivecpmnetwork.com/ec0487cde03d79b75629df8828d753f9/invoke.js';
 
-export const AD_EVERY_NTH_SUBHEADING = 1;
-export const MAX_ADS_PER_PAGE        = 8;
+export const AD_EVERY_NTH_SUBHEADING = 2;
+export const MAX_ADS_PER_PAGE        = 5;
 
 export const AFFILIATE_ADS_ENABLED = true;
 export const AFFILIATE_ADS = [
@@ -27,7 +27,7 @@ export const AFFILIATE_ADS = [
 
 ];
 
-export const CUSTOM_AD_ENABLED = false;
+export const CUSTOM_AD_ENABLED = true;
 export const CUSTOM_ADS = [
   {
     image:       '/images/ads/doxiee-affiliate-masterclass.jpg',
