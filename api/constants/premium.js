444b0a759deec78ad1dd786288b5e018ac87f056
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2026-05-18 01:55:54 +0100

    Update PRO monthly price from 100 to 3000

--- api/constants/premium.js
+++ api/constants/premium.js
@@ -8,7 +8,7 @@ export const FREE_COOLDOWN_HOURS   = 0;
 
 export const PREMIUM_MONTHLY_PRICE = 9000;
 export const PREMIUM_ANNUAL_PRICE  = 89000;
-export const PRO_MONTHLY_PRICE     = 100;
+export const PRO_MONTHLY_PRICE     = 3000;
 
 export const PAYMENT_URL_PRO       = 'https://selar.co/elitescholars-pro';
 export const PAYMENT_URL_MONTHLY   = 'https://selar.co/elitescholars-monthly';
